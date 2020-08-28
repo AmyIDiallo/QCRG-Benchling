78d66bc39750ce152e32dd111043d53ef68b0ae2
Amy Diallo <70337829+AmyIDiallo@users.noreply.github.com>
2020-08-27 17:37:23 -0700

    Create QCRG registry dashboard.sql

---
+++ Benchling use overview/QCRG registry dashboard.sql
@@ -0,0 +1,11 @@
+SELECT 
+    distinct count(*) OVER (PARTITION by a.creator_id, type),
+    COALESCE(c.name, 'ts_gk8SHbFg') as "Register",
+    b.name as "User"
+    
+FROM qcrg.registry_entity$raw as a
+left join user$raw as b on a.creator_id = b.id
+left join qcrg.schema as c on c.id = a.schema_id
+left JOIN project$raw d on d.id = a.source_id
+where not c.name = 'HEK Pellet' and not c.name = 'Crystal'
+ORDER BY b.name ASC
