8f4bd8d3d0411447eb8de5420c076153a8a5799b
Amy Diallo <70337829+AmyIDiallo@users.noreply.github.com>
2020-08-27 18:26:14 -0700

    Update Protein purified.sql

--- Purification team dashboard/Protein purified.sql
+++ Purification team dashboard/Protein purified.sql
@@ -1,13 +1,10 @@
 SELECT 
-    CAST(a.created_at$ AS DATE) as "Prep date",
-    a.id as "Protein prep ID",
-    a.protein as "Protein",
-    a.expressing_line as "Strain ID",
-    a.id as "Notebook",
-    a.concentration_mgml as "Concentration (mg/mL)",
-    a.buffer as "Buffer",
-    b.name
+    CAST(created_at$ AS DATE) as "Prep date",
+    id as "Protein prep ID",
+    protein as "Protein",
+    expressing_line as "Strain ID",
+    concentration_mgml as "Concentration (mg/mL)",
+    buffer as "Buffer"
     
-from qcrg.purified_protein as a
-inner join (select id, name from qcrg.user) as b on a.creator_id$ = b.id
+from qcrg.purified_protein
 order by created_at$ desc
