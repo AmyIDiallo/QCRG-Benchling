ca7dcf62e67a496996d879125032a81a394ab7d5
Amy Diallo <70337829+AmyIDiallo@users.noreply.github.com>
2020-08-27 18:12:40 -0700

    Update Beamtime.sql

--- Xray team dashboard/Beamtime.sql
+++ Xray team dashboard/Beamtime.sql
@@ -2,11 +2,12 @@ SELECT
     CAST(a.created_at$ AS DATE) as "Date",
     c.protein as "Protein",
     c.id as "Tray",
+    count(a.data) as "Crystal tested",
     a.entry_id$ as "Notebook",
-    b.name as "User",
-    a.data as "Data"
+    b.name as "User"
 
 FROM qcrg.beamtime as a
-left join user$raw as b on a.creator_id$ = b.id
-left join qcrg.crystal_tray as c on a.tray = c.crystal_plate_id
-order by a.date desc
+left join (select name, id from user$raw) as b on a.creator_id$ = b.id
+left join (select protein, id, crystal_plate_id from qcrg.crystal_tray) as c on a.tray = c.crystal_plate_id
+group by CAST(a.created_at$ AS DATE), c.protein, c.id,a.entry_id$, b.name
+order by CAST(a.created_at$ AS DATE) desc
