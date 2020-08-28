b1f8e9abe48890340373e581786e1e26918782cc
Amy Diallo <70337829+AmyIDiallo@users.noreply.github.com>
2020-08-28 15:36:32 -0700

    Update and rename Grids single protein.sql to Single protein grids.sql

--- CryoEM team dashboard/Single protein grids.sql
+++ CryoEM team dashboard/Single protein grids.sql
@@ -1,12 +1,12 @@
 SELECT 
-    c.id as "Grid ID",
     CAST(c.created_at$ AS DATE) as "Freezing date",
+    c.id as "Grid ID",
     c.purified_protein_prep as "Protein prep ID",
     a.box_id as "Grid box ID",
     concat((a.row_index+1),'_',(a.column_index+1)) as "Row_Column"
 
 FROM qcrg.container as a
-inner join qcrg.container_content as b on b.container_id = a.id
-inner join qcrg.grid_cryoEM as c on c.id=b.entity_id
+inner join (select container_id, entity_id from qcrg.container_content) as b on b.container_id = a.id
+inner join (select created_at$, id, purified_protein_prep, purified_complex from qcrg.grid_cryoEM) as c on c.id=b.entity_id
 where a.schema_id ='consch_tRq18kii' and c.purified_complex is null
 order by c.created_at$ desc
