bf73466e674daed423fbc583bead597fd827fb8a
Amy Diallo <70337829+AmyIDiallo@users.noreply.github.com>
2020-08-27 18:28:00 -0700

    Update Complex purified.sql

--- Purification team dashboard/Complex purified.sql
+++ Purification team dashboard/Complex purified.sql
@@ -1,10 +1,10 @@
 SELECT 
+    CAST(created_at$ AS DATE) as "Prep date",
     id as "Complex prep ID",
     protein1_viral as "Viral protein",
     expressing_line as "Viral strain ID",
     protein2_host as "Host protein",
     expressing_line2 as "Host strain ID",
-    CAST(created_at$ AS DATE) as "Prep date",
     concentration_mgml as "Concentration (mg/mL)",
     buffer_field as "Buffer"
     
