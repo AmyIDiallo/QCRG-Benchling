0fdb5cd2f603340575f1ce86e873e78268d6274a
Amy Diallo <70337829+AmyIDiallo@users.noreply.github.com>
2020-08-31 17:38:09 -0700

    Create Purification - Lookup construct informations.sql

---
+++ Result Schemas/Purification - Lookup construct informations.sql
@@ -0,0 +1,53 @@
+{
+    "fields": [
+        {
+            "isMulti": false,
+            "type": "entity_link",
+            "displayName": "Entity",
+            "name": "entity",
+            "isRequired": true
+        },
+        {
+            "isMulti": false,
+            "type": "text",
+            "displayName": "Protein name",
+            "name": "mame",
+            "isRequired": true
+        },
+        {
+            "isMulti": false,
+            "type": "text",
+            "displayName": "Molecular weight",
+            "name": "mw",
+            "isRequired": true
+        },
+        {
+            "isMulti": false,
+            "type": "text",
+            "displayName": "Tag",
+            "name": "tag",
+            "isRequired": true
+        },
+        {
+            "isMulti": false,
+            "type": "text",
+            "displayName": "Cleavage site",
+            "name": "cleavage",
+            "isRequired": true
+        },
+        {
+            "isMulti": false,
+            "type": "text",
+            "displayName": "Molecular weight 2",
+            "name": "mw2",
+            "isRequired": false
+        },
+        {
+            "isMulti": false,
+            "type": "text",
+            "displayName": "Molecular weight 3",
+            "name": "mw3",
+            "isRequired": false
+        }
+    ]
+}
