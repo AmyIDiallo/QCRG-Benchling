11d205fd358b5116afd2ea5e2d9b4723e3aee453
Amy Diallo <70337829+AmyIDiallo@users.noreply.github.com>
2020-12-09 12:34:10 -0800

    Update Pull down assay.sql

--- E. coli team dashboard/Pull down assay.sql
+++ E. coli team dashboard/Pull down assay.sql
@@ -1,7 +1,6 @@
 SELECT
     strainviral as "Viral protein",
     strainhost as "Host protein",
-    strainviral as "Viral protein",
     interaction as "Interaction validation",
     entry_id$ as "Notebook"
 FROM qcrg.ecoli_pull_down_assay
